c6880629f0bf03045bddc59c8d0c02be5790307b
svistylin <37051266+svistylin@users.noreply.github.com>
2019-06-25 23:26:30 +0300

    Update CounterOfFrequency.java

--- reader/CounterOfFrequency.java
+++ reader/CounterOfFrequency.java
@@ -5,12 +5,19 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CounterOfFrequency {
+
     public static void counterOfFrequency(String string) {
         Arrays.stream(string.toLowerCase().split("[^\\p{L}\\p{Digit}_]+"))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
-                .sorted((o1, o2) -> new Comparator().compare(o1, o2))
+                .sorted((a, b) -> {
+                    int comp = b.getValue().compareTo(a.getValue());
+                    if (comp == 0) {
+                        return a.getKey().compareTo(b.getKey());
+                    }
+                    return comp;
+                })
                 .limit(10)
                 .forEach(stringLongEntry -> System.out.println(stringLongEntry.getKey()));
     }
