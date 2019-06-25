847bc92906304bd13a8d1177cdaeb3dcddf85347
svistylin <37051266+svistylin@users.noreply.github.com>
2019-06-25 12:21:39 +0300

    Update Comparator.java

--- reader/Comparator.java
+++ reader/Comparator.java
@@ -5,13 +5,6 @@ import java.util.Map;
 public class Comparator implements java.util.Comparator<Map.Entry<String, Long>> {
     @Override
     public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
-        if (o2.getValue() > o1.getValue()) {
-            return 1;
-        }
-        if (o2.getValue() < o1.getValue()) {
-            return -1;
-        } else {
-            return o1.getKey().compareTo(o2.getKey());
-        }
+        return (o1.getValue()!=o2.getValue())? (int) (o2.getValue() - o1.getValue()) :o1.getKey().compareTo(o2.getKey());
     }
 }
