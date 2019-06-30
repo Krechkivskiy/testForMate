5f8c15f1ba8c465226235de4dc41d24a5855a8fe
svistylin <37051266+svistylin@users.noreply.github.com>
2019-06-30 22:53:37 +0300

    solved cs problems

---
+++ reflection/Main.java
@@ -0,0 +1,20 @@
+package reflection;
+
+import reflection.controller.UserConsoleHandler;
+import reflection.lib.Injector;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Injector.injectDependency();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println("please enter across whitespace users id and users name");
+        UserConsoleHandler.handdle();
+    }
+}
+
+
+
+
