8f02385f4fbb74fc3e1a20f4563f27af6ee0323b
svistylin <37051266+svistylin@users.noreply.github.com>
2019-07-03 11:53:25 +0300

    Update Main.java

--- main/java/main/Main.java
+++ main/java/main/Main.java
@@ -3,14 +3,14 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SIngOutServlet;
+
 import servlets.SingInServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new SingInServlet()), "/signin");
-        context.addServlet(new ServletHolder(new SIngOutServlet()), "/signup");
+        context.addServlet(new ServletHolder(new SingInServlet()), "/signup");
         Server server = new Server(8080);
         server.setHandler(context);
         server.start();
