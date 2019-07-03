ce68addce188b4b2026c06d204f45ab2feceedd0
svistylin <37051266+svistylin@users.noreply.github.com>
2019-07-04 01:50:57 +0300

    Update Main.java

--- main/java/main/Main.java
+++ main/java/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
 import servlets.SingInServlet;
 
 public class Main {
