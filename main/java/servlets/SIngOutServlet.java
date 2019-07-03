06abc9e1af33c3479c40632914a0da19395fc697
svistylin <37051266+svistylin@users.noreply.github.com>
2019-07-03 11:54:45 +0300

    Update SIngOutServlet.java

--- main/java/servlets/SIngOutServlet.java
+++ main/java/servlets/SIngOutServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import dao.UserDao;
+import dao.UserDaoImpl;
 import model.UserModel;
 
 import javax.servlet.ServletException;
@@ -9,10 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class SIngOutServlet extends HttpServlet {
+public class SingOutServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UserDao userDao = new UserDao();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserDao userDao = new UserDaoImpl();
         userDao.addUser(new UserModel(request.getParameter("login"), request.getParameter("password")));
     }
 }
