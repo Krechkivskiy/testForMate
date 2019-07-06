914a70219194811d890676d5d4354cf2fa52b920
svistylin <37051266+svistylin@users.noreply.github.com>
2019-07-06 17:59:06 +0300

    Update LogInServlet.java

--- src/main/java/controller/LogInServlet.java
+++ src/main/java/controller/LogInServlet.java
@@ -13,10 +13,6 @@ import java.util.List;
 
 @WebServlet("/register")
 public class LogInServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-    }
 
     @Override
     protected void doPost(HttpServletRequest request,
